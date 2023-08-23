d5b8ab0e502280300fa3eb1b233290e3e94e653a
L3w15DM <117673970+L3w15DM@users.noreply.github.com>
2023-08-22 19:01:07 -0500

    add files via upload

---
+++ Wallet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    internal class Wallet
+    {
+        public double money;
+
+        public Wallet(double money)
+        {
+            //TODO 1: Assign the value of the 'money' parameter to the 'money' instance variable. One line.
+            this.money = money;
+        }
+    }
+}
